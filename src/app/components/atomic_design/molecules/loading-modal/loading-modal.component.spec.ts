34d2aca7f7d26238da0a875b775b7740f81f3605
Juan Miguel Rojas <juan.noriega@correounivalle.edu.co>
2024-05-20 12:38:16 -0500

    Se realizan pruebas unitarias, se da por finalizada la HU1

--- src/app/components/atomic_design/molecules/loading-modal/loading-modal.component.spec.ts
+++ src/app/components/atomic_design/molecules/loading-modal/loading-modal.component.spec.ts
@@ -1,23 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoadingModalComponent } from './loading-modal.component';
+import { Store, StoreModule } from '@ngrx/store';
+import { LoadingModalState } from 'src/app/components/core/models/loading-modal.state';
+import { of } from 'rxjs';
+import { startLoading } from 'src/app/state/actions/loading-modal.action';
 
 describe('LoadingModalComponent', () => {
   let component: LoadingModalComponent;
   let fixture: ComponentFixture<LoadingModalComponent>;
+  let store: Store<{ isLoading: LoadingModalState }>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoadingModalComponent ]
+      declarations: [ LoadingModalComponent ],
+      imports: [StoreModule.forRoot({})],
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoadingModalComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+    spyOn(store, 'select').and.returnValue(of(true));
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create loading-modal', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start loading on init', () => {
+    expect(store.dispatch).toHaveBeenCalledWith(startLoading());
+  });
+
+  it('should render loading modal when isLoading$ is true', () => {
+    const loadingModalElement: HTMLElement = fixture.nativeElement.querySelector('.loading-modal');
+    expect(loadingModalElement).toBeTruthy();
+  });
+
+  it('should not render loading modal when isLoading$ is false', () => {
+    fixture.detectChanges();
+    const loadingModalElement: HTMLElement = fixture.nativeElement.querySelector('.loading-modal');
+    expect(!loadingModalElement).toBeFalsy();
+  });
+
 });
--- src/app/components/atomic_design/molecules/loading-modal/loading-modal.component.ts
+++ src/app/components/atomic_design/molecules/loading-modal/loading-modal.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Store} from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable} from 'rxjs';
 import { LoadingModalState } from 'src/app/components/core/models/loading-modal.state';
 import { startLoading } from 'src/app/state/actions/loading-modal.action';
 import { selectIsLoadingSelector } from 'src/app/state/selectors/loading-modal.selector';
@@ -14,17 +14,16 @@ export class LoadingModalComponent implements OnInit {
 
   isLoading$: Observable<boolean>;
 
-  constructor(private store : Store<{isLoading: LoadingModalState}>) {
+
+  constructor(private store : Store<{isLoading: LoadingModalState}>, private cdr : ChangeDetectorRef) {
     this.isLoading$ = new Observable<boolean>();
    }
 
   ngOnInit(): void {
     this.isLoading$ = this.store.select(selectIsLoadingSelector);
+     this.loadPage();
   }
 
-  ngAfterViewInit() {
-    this.loadPage();
-  }
 
   loadPage() {
     this.store.dispatch(startLoading());
